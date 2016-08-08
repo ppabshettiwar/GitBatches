0337c1b5694d023bd295a550f261123ec376d4e2
poojapabshettiwar <pooja.pabshettiwar@enzigma.com>
2016-08-08 18:09:38 +0530

    --Commiting changes manually

--- CompressAndCommit.bat
+++ CompressAndCommit.bat
@@ -11,31 +11,30 @@ mkdir application_js_min\debug
 mkdir application_css_min
 mkdir application_css_min\img
 
-copy application_js\templates\*.* application_js_min\templates\
-for /f "delims=|" %%f in ('dir /b application_js\*.js' ) do java -jar yuicompressor-2.4.8.jar --type js -o application_js_min\%%f application_js\%%f
-copy application_js\*.js application_js_min\debug\
+	copy application_js\templates\*.* application_js_min\templates\
+	for /f "delims=|" %%f in ('dir /b application_js\*.js' ) do java -jar yuicompressor-2.4.8.jar --type js -o application_js_min\%%f application_js\%%f
+	copy application_js\*.js application_js_min\debug\
 
-copy application_css\img\*.* application_css_min\img\
-rem for /f "delims=|" %%f in ('dir /b application_css\*.css' ) do java -jar yuicompressor-2.4.8.jar --nomunge --type css -o application_css_min\%%f application_css\%%f
-copy application_css\*.* application_css_min\
+	copy application_css\img\*.* application_css_min\img\
+		rem for /f "delims=|" %%f in ('dir /b application_css\*.css' ) do java -jar yuicompressor-2.4.8.jar --nomunge --type css -o application_css_min\%%f application_css\%%f
+	copy application_css\*.* application_css_min\
 
-cd application_js
-%GIT_PATH% ls-files -z | xargs -0 %GIT_PATH% update-index --assume-unchanged
+	cd application_js
+	%GIT_PATH% ls-files -z | xargs -0 %GIT_PATH% update-index --assume-unchanged
 
-cd..
-cd application_css
-%GIT_PATH% ls-files -z | xargs -0 %GIT_PATH% update-index --assume-unchanged
-set /p Comment="Enter  Comment to commit: "
-cd..
-	%GIT_PATH% add -A
-	%GIT_PATH% pull origin devleoper
-	%GIT_PATH% commit -m "%Comment%"
-	%GIT_PATH% push origin devleoper
-
+	cd..
+	cd application_css
+	%GIT_PATH% ls-files -z | xargs -0 %GIT_PATH% update-index --assume-unchanged
+	
+	set /p Comment="Enter  Comment to commit: "
+	cd..	
 	%GIT_PATH% checkout master
 	%GIT_PATH% pull %MASTERBRANCH%
 	%GIT_PATH% merge devleoper
 	%GIT_PATH% push %MASTERBRANCH%
  	
 	%GIT_PATH% checkout devleoper
+	rmdir /S /Q application_js_min
+	rmdir /S /Q application_css_min
+
 pause
